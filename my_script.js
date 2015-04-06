ca9083e686edf9178ca7851c9c76522afdc0ae93
NUS <nuspc@CNX03600PJ.(none)>
2015-04-06 18:15:59 +0700

    v0.1

---
+++ my_script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$("#hhh").mouseenter(function(){
+		$("#myModal").modal('show');
+
+	});
+
+	$("#u19vn").click(function(){
+		$("#u19").modal('show');
+
+	});
+	
+
+});
