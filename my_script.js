4b8df614415448297435d041841b0e38be9c9f6f
NUS <nuspc@SGH003T03L.(none)>
2015-04-09 16:34:32 +0700

    validation

--- my_script.js
+++ my_script.js
@@ -1,17 +1,80 @@
 $(document).ready(function(){
-	$("#hhh").mouseenter(function(){
-		$("#myModal").modal('show');
 
-	});
+  $("#submit").click(function(){
+    $(this).showLoading("toggle");
+  });
 
-	$("#u19vn").click(function(){
-		$("#u19").modal('show');
+  $("#hhh").mouseenter(function(){
+    $("#myModal").modal('show');
+  });
 
-	});
-	$("#signup").click(function(){
-		$("#sign_up").modal('show');
+  $("#u19vn").click(function(){
+    $("#u19").modal('show');
 
-	});
-	
+  });
 
+  $("#signup").click(function(){
+    $("#sign_up").modal('show');
+  });
+  
+  //validation
+  $.validator.setDefaults({
+    submitHandler: function() {
+      alert("submitted!");
+    }
+  });
+
+
+  $("#signupForm").validate({
+      rules: {
+        firstname: "required",
+        lastname: "required",
+        username: {
+          required: true,
+          minlength: 2
+        },
+        password: {
+          required: true,
+          minlength: 8
+        },
+        confirm_password: {
+          required: true,
+          minlength: 8,
+          equalTo: "#password"
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        topic: {
+          required: "#newsletter:checked",
+          minlength: 2
+        },
+        agree: "required"
+      },
+      messages: {
+        firstname: "Please enter your firstname",
+        lastname: "Please enter your lastname",
+        username: {
+          required: "Please enter a username",
+          minlength: "Your username must consist of at least 2 characters"
+        },
+        password: {
+          required: "Please provide a password",
+          minlength: "Your password must be at least 8 characters long"
+        },
+        confirm_password: {
+          required: "Please provide a password",
+          minlength: "Your password must be at least 8 characters long",
+          equalTo: "Please enter the same password as above"
+        },
+        email: "Please enter a valid email address",
+        agree: "Please accept our policy"
+      }
+    });
+
+  //show loading
+
+  
 });
+
---
+++ showloading/my_script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+  $("sd").click(function(){
+    $(this).showLoading();
+  });   
+     
+});
